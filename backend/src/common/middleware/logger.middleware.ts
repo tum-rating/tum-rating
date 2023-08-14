9949ad21f1b11c6689dd9f889c67d05d1cc3e451
Waldemar Zieliński <waldemar99zielinski@gmail.com>
2023-08-14 21:39:06 +0200

    improv(backend): pino logger

---
+++ backend/src/common/middleware/logger.middleware.ts
@@ -0,0 +1,13 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { randomUUID } from 'crypto';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    if(!req.headers['x-trace-id'])
+        req.headers['x-trace-id'] = randomUUID();
+    console.log('middleware, x-trace-id', req.headers['x-trace-id'])
+    next();
+  }
+}
