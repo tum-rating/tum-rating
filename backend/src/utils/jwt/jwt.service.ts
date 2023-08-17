2ff34e6d836fbacf98fed986c0ec191e5c5f0550
Waldemar Zieliński <waldemar99zielinski@gmail.com>
2023-08-17 21:54:58 +0200

    refactor(all): rebase align

--- backend/src/utils/jwt/jwt.service.ts
+++ backend/src/utils/jwt/jwt.service.ts
@@ -6,62 +6,14 @@ import { TokenType } from './jwt.interfaces';
 
 @Injectable()
 export class JWTService {
-  private readonly _jwtSecret: Uint8Array;
+    private readonly _jwtSecret: Uint8Array;
 
-  constructor(private readonly _configService: ConfigService) {
-    const jwtSecretString = this._configService.getOrThrow('jwt.secret');
-    this._jwtSecret = new TextEncoder().encode(jwtSecretString);
-  }
-
-  public async signJWTAccess(userId: string) {
-    return this._signJWT(userId, TokenType.access, '1d');
-  }
-
-  public async verifyJWTAccess(token: string) {
-    return this._verifyJWT(token, TokenType.access);
-  }
-
-  public async signJWTActivate(userId: string) {
-    return this._signJWT(userId, TokenType.activation, '1d');
-  }
-
-  public async signJWTRecovery(userId: string) {
-    return this._signJWT(userId, TokenType.recovery, '1d');
-  }
-
-  public async verifyJWTActivate(token: string) {
-    return this._verifyJWT(token, TokenType.activation);
-  }
-
-  private async _signJWT(
-    userId: string,
-    tokenType: TokenType,
-    expiration: string,
-  ) {
-    const token = await new SignJWT({ tokenType })
-      .setProtectedHeader({ alg: 'HS256' })
-      .setSubject(userId)
-      .setExpirationTime(expiration)
-      .sign(this._jwtSecret);
-
-    return token;
-  }
-
-  private async _verifyJWT(token: string, tokenType: TokenType) {
-    try {
-      const { payload, protectedHeader } = await jwtVerify(
-        token,
-        this._jwtSecret,
-      );
-
-      if (payload.tokenType !== tokenType)
-        return { isValid: false, payload: null };
-
-      return { isValid: true, payload };
-    } catch (error) {
-      return { isValid: false, payload: null };
+    constructor (
+        private readonly _configService: ConfigService,
+    ) {
+        const jwtSecretString = this._configService.getOrThrow('jwt.secret');
+        this._jwtSecret = new TextEncoder().encode(jwtSecretString);
     }
-<<<<<<< HEAD
 
     public async signJWTAccess(userId: string) {
         return this._signJWT(userId, TokenType.access, '1d');
@@ -110,7 +62,3 @@ export class JWTService {
         }
     }
 }
-=======
-  }
-}
->>>>>>> 279dc92 (feat(frontend):activate,modals-manager,404page)
