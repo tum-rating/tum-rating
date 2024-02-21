2ee1248149d5c4a9ed3066ad53cbfbdaa8249ee6
JRzankowski <j.rzankowski@gmail.com>
2024-02-21 16:35:10 +0100

    wip(front): table styling

--- frontend/src/components/ClassesTable/ClassesTable.tsx
+++ frontend/src/components/ClassesTable/ClassesTable.tsx
@@ -1,13 +1,14 @@
-import { DataTable } from 'mantine-datatable';
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { columns } from '@/components/ClassesTable/Columns';
-import { Review } from '@/reviews/types';
-import { usePaginatedReviews } from '@/reviews/usePaginatedReviews';
-import { useNavigate } from 'react-router-dom';
-import { useViewportSize } from '@mantine/hooks';
+import {DataTable} from 'mantine-datatable';
+import {useEffect, useMemo, useRef, useState} from 'react';
+import {columns} from '@/components/ClassesTable/Columns';
+import {Review} from '@/reviews/types';
+import {usePaginatedReviews} from '@/reviews/usePaginatedReviews';
+import {useNavigate} from 'react-router-dom';
+import {useViewportSize} from '@mantine/hooks';
 import classes from './ClassesTable.module.css';
-import { useTableScrollContext } from '@/context';
-import { isMobile } from 'react-device-detect';
+import {useTableScrollContext} from '@/context';
+import {isMobile} from 'react-device-detect';
+import {Box, Flex, Skeleton, Text} from "@mantine/core";
 
 const ClassesTable = () => {
     const columnsConfiguration = useMemo(() => {
@@ -18,11 +19,11 @@ const ClassesTable = () => {
     }, []);
     const [records, setRecords] = useState<Review[]>([]);
     const [internalLoading, setInternalLoading] = useState(true);
-    const { data, fetchNextPage, isFetching } = usePaginatedReviews();
-    const { height } = useViewportSize();
+    const {data, fetchNextPage, isFetching} = usePaginatedReviews();
+    const {height} = useViewportSize();
     const navigate = useNavigate();
     const scrollViewportRef = useRef<HTMLDivElement>(null);
-    const { scrollY, setScrollY } = useTableScrollContext();
+    const {scrollY, setScrollY} = useTableScrollContext();
 
     useEffect(() => {
         if (data) {
@@ -39,7 +40,8 @@ const ClassesTable = () => {
     }, [scrollViewportRef.current]);
 
     const loadMoreRecords = () => {
-        fetchNextPage().then(() => {});
+        fetchNextPage().then(() => {
+        });
     };
 
     const handleRowClick = (record: Review) => {
@@ -47,12 +49,41 @@ const ClassesTable = () => {
         setScrollY(scrollViewportRef.current.scrollTop);
         navigate(dynamicPath);
     };
-
     return (
         <>
-            <DataTable withColumnBorders highlightOnHover striped height={height - 45} columns={columnsConfiguration} records={records} onScrollToBottom={loadMoreRecords} scrollViewportRef={scrollViewportRef} fetching={isFetching || internalLoading} className={classes.dataTable} rowClassName={classes.dataTableRow} onRowClick={({ record }) => handleRowClick(record)}></DataTable>
+            <Box className={classes.dataTableContainer} h={height - 250}>
+                <Flex justify="space-between" align="center" className={classes.dataTableInfo}>
+                    <Flex></Flex>
+                    <Flex>
+                        <Text fw="500" size="xs">
+                            Courses loaded:
+                        </Text>
+                        <Text
+                            ml="5"
+                            size="xs"
+                            fw="bold"
+                            c="blue">
+                            {isFetching || records.length === 0 ? <Skeleton w={20} h={15}/> : records.length}
+                        </Text>
+                    </Flex>
+                </Flex>
+                <DataTable withRowBorders={false}
+                           highlightOnHover
+                           striped
+                           verticalSpacing="lg"
+                           height="100%"
+                           columns={columnsConfiguration}
+                           records={records}
+                           onScrollToBottom={loadMoreRecords}
+                           scrollViewportRef={scrollViewportRef}
+                           fetching={isFetching || internalLoading}
+                           className={classes.dataTable}
+                           rowClassName={classes.dataTableRow}
+                           onRowClick={({record}) => handleRowClick(record)}></DataTable>
+            </Box>
+
         </>
     );
 };
 
-export { ClassesTable };
+export {ClassesTable};
--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,43 +1,72 @@
-import { PropsWithChildren } from 'react';
-import { ActionIcon, Anchor, AppShell, Box, Burger, Button, Drawer, Flex, Group, Image, Stack, Switch, Text, useMantineColorScheme } from '@mantine/core';
-import { useDisclosure, useHotkeys } from '@mantine/hooks';
+import {PropsWithChildren} from 'react';
+import {
+    ActionIcon,
+    Anchor,
+    AppShell,
+    Burger,
+    Button,
+    Drawer,
+    Flex,
+    getGradient,
+    Group,
+    Image,
+    Stack,
+    Switch,
+    Text,
+    useMantineColorScheme,
+    useMantineTheme
+} from '@mantine/core';
+import {useDisclosure, useHotkeys} from '@mantine/hooks';
 import logo from '@/assets/img/logo.png';
-import { IconMoonStars, IconSearch, IconSun } from '@tabler/icons-react';
-import { useUser } from '@/auth/useUser';
-import { UserButton } from '@/components/UserButton';
-import { useSignOut } from '@/auth/useSignOut';
-import { SpotlightControl } from '@/components/Spotlight/SpotlightControl';
-import { getPath, Paths } from '@/routes/paths.ts';
-import { useNavigate } from 'react-router-dom';
+import {IconMoonStars, IconSearch, IconSun} from '@tabler/icons-react';
+import {useUser} from '@/auth/useUser';
+import {UserButton} from '@/components/UserButton';
+import {useSignOut} from '@/auth/useSignOut';
+import {SpotlightControl} from '@/components/Spotlight/SpotlightControl';
+import {getPath, Paths} from '@/routes/paths.ts';
+import {useNavigate} from 'react-router-dom';
 
-export const MainLayout = ({ children }: PropsWithChildren) => {
-    const { user } = useUser();
+import {useGradient} from "@/hooks";
+
+
+const HEADER_HEIGHT = 45;
+const MAX_SITE_WIDTH = 1320;
+
+export const MainLayout = ({children}: PropsWithChildren) => {
+    const {user} = useUser();
     const navigate = useNavigate();
-    const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
-    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+    const [mobileOpened, {toggle: toggleMobile}] = useDisclosure();
+    const theme = useMantineTheme();
+    const {colorScheme, toggleColorScheme} = useMantineColorScheme();
     const signOut = useSignOut();
+
+
+    const {layoutGradient, primaryGradient} = useGradient();
+
+
     useHotkeys([['/', () => navigate(getPath(Paths.spotlight))]]);
     const Logo = () => (
         <Anchor href="/">
-            <Image data-test="app-logo" fit="contain" height={28} width={129} src={logo} alt="tum rating logo" />
+            <Image data-test="app-logo" fit="contain" height={28} width={129} src={logo} alt="tum rating logo"/>
         </Anchor>
     );
     return (
-        <AppShell header={{ height: 45 }} padding="md">
+        <AppShell header={{height: HEADER_HEIGHT}} padding="md">
             <AppShell.Header maw="100vw">
                 <Group visibleFrom="sm" h="100%" px="md" justify="space-between">
-                    <Logo />
-                    <SpotlightControl onClick={() => navigate(getPath(Paths.spotlight))} />
+                    <Logo/>
+                    <SpotlightControl onClick={() => navigate(getPath(Paths.spotlight))}/>
                     {!user ? (
                         <>
-                            <Button data-testid="cypress-open-sign-in-modal-btn" size="xs" variant="outline" onClick={() => navigate(getPath(Paths.signIn))}>
+                            <Button data-testid="cypress-open-sign-in-modal-btn" size="xs" variant="outline"
+                                    onClick={() => navigate(getPath(Paths.signIn))}>
                                 Sign In
                             </Button>
                             <Button
                                 data-testid="cypress-open-sign-up-modal-btn"
                                 size="xs"
                                 variant="gradient"
-                                gradient={{ from: 'indigo', to: 'blue', deg: 90 }}
+                                gradient={primaryGradient}
                                 onClick={() => {
                                     navigate(getPath(Paths.signUp));
                                 }}
@@ -47,25 +76,28 @@ export const MainLayout = ({ children }: PropsWithChildren) => {
                         </>
                     ) : null}
 
-                    <UserButton />
+                    <UserButton/>
                     <ActionIcon variant="outline" onClick={toggleColorScheme}>
-                        {colorScheme === 'dark' ? <IconSun size="1.1rem" /> : <IconMoonStars size="1.1rem" />}
+                        {colorScheme === 'dark' ? <IconSun size="1.1rem"/> : <IconMoonStars size="1.1rem"/>}
                     </ActionIcon>
                 </Group>
                 <Group hiddenFrom="sm" h="100%" px="md" justify="space-between">
-                    <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-                    <Logo />
-                    <ActionIcon variant="outline" data-testid="cypress-global-search" onClick={() => navigate(getPath(Paths.spotlight))}>
-                        <IconSearch size="1.1rem" />
+                    <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm"/>
+                    <Logo/>
+                    <ActionIcon variant="outline" data-testid="cypress-global-search"
+                                onClick={() => navigate(getPath(Paths.spotlight))}>
+                        <IconSearch size="1.1rem"/>
                     </ActionIcon>
                 </Group>
             </AppShell.Header>
-            <AppShell.Main p={0} m={0}>
-                <Drawer style={{ zIndex: 6 }} title={<Logo />} opened={mobileOpened} onClose={toggleMobile} overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}>
+            <AppShell.Main p={0} m={0} bg={getGradient(layoutGradient, theme)}>
+                <Drawer style={{zIndex: 6}} title={<Logo/>} opened={mobileOpened} onClose={toggleMobile}
+                        overlayProps={{backgroundOpacity: 0.5, blur: 4}}>
                     <Stack h="100%" justify="space-between">
                         <Flex align="center" justify="space-between">
-                            {user ? <UserButton withoutDropdown /> : <Text>Hello</Text>}
-                            <Switch size="md" onChange={toggleColorScheme} checked={colorScheme === 'light'} onLabel={<IconSun size="1.1rem" />} offLabel={<IconMoonStars size="1.1rem" />} />
+                            {user ? <UserButton withoutDropdown/> : <Text>Hello</Text>}
+                            <Switch size="md" onChange={toggleColorScheme} checked={colorScheme === 'light'}
+                                    onLabel={<IconSun size="1.1rem"/>} offLabel={<IconMoonStars size="1.1rem"/>}/>
                         </Flex>
                         <Flex direction="column" w="100%" wrap="nowrap" gap="sm">
                             {user ? (
@@ -74,10 +106,13 @@ export const MainLayout = ({ children }: PropsWithChildren) => {
                                 </Button>
                             ) : (
                                 <>
-                                    <Button data-testid="cypress-open-sign-in-modal-btn" fullWidth size="md" variant="outline" onClick={() => navigate(getPath(Paths.signIn))}>
+                                    <Button data-testid="cypress-open-sign-in-modal-btn" fullWidth size="md"
+                                            variant="outline" onClick={() => navigate(getPath(Paths.signIn))}>
                                         Sign In
                                     </Button>
-                                    <Button data-testid="cypress-open-sign-up-modal-btn" fullWidth size="md" variant="gradient" gradient={{ from: 'indigo', to: 'blue', deg: 90 }} onClick={() => navigate(getPath(Paths.signUp))}>
+                                    <Button data-testid="cypress-open-sign-up-modal-btn" fullWidth size="md"
+                                            variant="gradient" gradient={primaryGradient}
+                                            onClick={() => navigate(getPath(Paths.signUp))}>
                                         Sign Up
                                     </Button>
                                 </>
@@ -85,7 +120,7 @@ export const MainLayout = ({ children }: PropsWithChildren) => {
                         </Flex>
                     </Stack>
                 </Drawer>
-                <Box pt={45}>{children}</Box>
+                <Flex justify="center" pt={HEADER_HEIGHT} mx="auto" h="100%" maw={MAX_SITE_WIDTH}>{children}</Flex>
             </AppShell.Main>
         </AppShell>
     );
