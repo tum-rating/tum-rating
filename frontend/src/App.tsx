3729ba50baeeeecac28c57fbbb42d0fb5c0af7e5
JRzankowski <j.rzankowski@gmail.com>
2024-02-28 16:50:49 +0100

    feat(frontend): add green gradient

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,14 +13,17 @@ import buttonClasses from './customStyles/Buttons.module.css'
 const resolver: CSSVariablesResolver = () => ({
     variables: {
         "--primary-gradient": `linear-gradient(90deg, var(--mantine-color-indigo-filled) 0%, var(--mantine-color-blue-filled) 100%);`,
+        "--green-gradient": `linear-gradient(170deg, var(--mantine-color-teal-filled) 0%, var(--mantine-color-lime-filled) 100%);`
     },
     light: {
         '--primary-layout-gradient': 'linear-gradient(90deg, var(--mantine-color-indigo-filled) 0%, var(--mantine-color-blue-filled) 100%)',
         '--primary-light-gradient': 'linear-gradient(90deg, rgba(145, 167, 255, 0.1) 0%, rgba(77, 171, 247, 0.1) 100%)',
+        '--green-light-gradient': 'linear-gradient(170deg, rgb(18 184 134 / 13%) 0%, rgb(130 201 30 / 13%) 100%)'
     },
     dark: {
         '--primary-layout-gradient': 'linear-gradient(90deg, var(--mantine-color-indigo-9) 0%, var(--mantine-color-blue-9) 100%)',
         '--primary-light-gradient': 'linear-gradient(90deg, rgba(145, 167, 255, 0.2) 0%, rgba(77, 171, 247, 0.2) 100%)',
+        '--green-light-gradient': 'linear-gradient(170deg, rgb(18 184 134 / 13%) 0%, rgb(130 201 30 / 13%) 100%)'
     },
 });
 
