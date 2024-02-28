4b1af4bb66206187ad387d8d34ca053c7cbcce13
JRzankowski <j.rzankowski@gmail.com>
2024-02-28 16:46:32 +0100

    feat(frontend): add Footer component

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import {Text,Container} from '@mantine/core';
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Text c="dimmed" size="sm">
+                    © 2024 tum-rating.de. All rights reserved.
+                </Text>
+            </Container>
+        </footer>
+
+    );
+}
+
+export {Footer}
