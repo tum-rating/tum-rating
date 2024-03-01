4175f7c2a669e4982cadd26474993c4f6b8b328a
JRzankowski <j.rzankowski@gmail.com>
2024-03-01 14:44:39 +0100

    feat(frontend): make search component responsive, make search controlled by URL when isMobileOnly

--- frontend/src/components/Search/SearchInputDesktop.tsx
+++ frontend/src/components/Search/SearchInputDesktop.tsx
@@ -1,14 +1,26 @@
-import {CloseButton, Combobox, Highlight, ScrollArea, Text, TextInput, ThemeIcon, useCombobox} from "@mantine/core";
-import {useEffect, useMemo, useState} from "react";
+import {
+    ActionIcon,
+    CloseButton,
+    Combobox,
+    Loader,
+    ScrollArea,
+    Text,
+    TextInput,
+    ThemeIcon,
+    useCombobox
+} from "@mantine/core";
+import {useEffect, useMemo, useRef, useState} from "react";
 import {useSearchReviews} from "@/reviews/useSearchReviews.tsx";
-import {useDebouncedState} from "@mantine/hooks";
+import {useDebouncedState, useMediaQuery} from "@mantine/hooks";
 import {Review} from "@/reviews/types.ts";
 
 import classes from "./SearchInputDesktop.module.css"
-import {IconSearch} from "@tabler/icons-react";
+import {IconArrowLeft, IconSearch} from "@tabler/icons-react";
 import clsx from "clsx";
-import {useNavigate} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import {SearchHighlight} from "@/components/Highlight";
+import {clearAllBodyScrollLocks, disableBodyScroll, enableBodyScroll} from "@/utils";
+import {isMobileOnly} from "react-device-detect";
 
 const SearchInputDesktop = () => {
     const combobox = useCombobox({
@@ -18,12 +30,38 @@ const SearchInputDesktop = () => {
     const [value, setValue] = useState('');
     const [empty, setEmpty] = useState(false);
     const [debouncedQuery, setDebouncedQuery] = useDebouncedState('', 150);
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+    const smallerMode = useMediaQuery('(max-width: 48em)');
     const navigate = useNavigate();
+    const location = useLocation();
+    const searchInputRef = useRef(null); // Create a ref for the search input
+
+    useEffect(() => {
+        if (searchInputRef.current) {
+            disableBodyScroll(searchInputRef.current);
+        }
+        return () => {
+            if (searchInputRef.current) {
+                enableBodyScroll(searchInputRef.current);
+            }
+            clearAllBodyScrollLocks();
+        };
+    }, []);
+
+
+    useEffect(() => {
+        if (isMobileOnly) {
+            setIsSearchOpen(location.hash === '#search');
+        }else{
+            setIsSearchOpen(false);
+        }
+    }, [location]);
 
 
     useEffect(() => {
         setDebouncedQuery(value);
     }, [value]);
+
     const {data} = useSearchReviews(debouncedQuery);
 
     const [previousData, setPreviousData] = useState(null);
@@ -44,7 +82,7 @@ const SearchInputDesktop = () => {
 
     const options = useMemo(() => {
         return (groupedActions || []).map((item: Review) => (
-            <Combobox.Option value={item.courseId} key={item.courseId}>
+            <Combobox.Option className={classes.option} value={item._id} key={item.courseId}>
                 <SearchHighlight value={value.split(' ')} text={item.course}/>
                 <SearchHighlight value={value.split(' ')} text={item.professor} textStyles={{
                     fz: "xs",
@@ -54,7 +92,6 @@ const SearchInputDesktop = () => {
             </Combobox.Option>
         ));
     }, [groupedActions]);
-
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -66,10 +103,103 @@ const SearchInputDesktop = () => {
 
     }
 
+    if (smallerMode) {
+        return (
+            <>
+                <ActionIcon variant="light" onClick={() => {
+                    if (isMobileOnly) {
+                        navigate('#search');
+                    } else {
+                        setIsSearchOpen(true);
+                    }
+
+                }}>
+                    <IconSearch width={16} height={16}/>
+                </ActionIcon>
+                {
+                    isSearchOpen && (
+                        <Combobox
+                            onOptionSubmit={(optionValue) => {
+                                navigate('/courses/' + optionValue);
+                                combobox.closeDropdown();
+                            }}
+
+                            offset={9}
+                            withinPortal={true}
+                            store={combobox}
+                        >
+                            <Combobox.EventsTarget>
+                                <form style={{width: "100%"}} onSubmit={handleSubmit}>
+                                    <TextInput
+                                        radius={0}
+                                        height={100}
+                                        size="xl"
+                                        leftSection={
+                                            <ActionIcon onClick={() => {
+                                                setIsSearchOpen(false)
+                                                combobox.closeDropdown();
+                                            }}>
+                                                <IconArrowLeft width={16} height={16}/>
+                                            </ActionIcon>
+                                        }
+                                        rightSection={
+                                            value !== '' && (
+                                                <CloseButton
+                                                    size="sm"
+                                                    onMouseDown={(event) => event.preventDefault()}
+                                                    onClick={() => {
+                                                        setValue('')
+                                                        combobox.closeDropdown()
+                                                    }}
+                                                    aria-label="Clear value"
+                                                />
+                                            )
+                                        }
+                                        classNames={{
+                                            root: classes.searchInputMobileRoot,
+                                            input: clsx(classes.searchInputMobileInput, combobox.dropdownOpened && classes.searchInputMobileInputActive),
+                                        }}
+                                        placeholder="Search..."
+                                        value={value}
+                                        onChange={(event) => {
+                                            setValue(event.currentTarget.value);
+                                            combobox.resetSelectedOption();
+                                            combobox.openDropdown();
+                                        }}
+                                        onBlur={() => {
+                                            setIsSearchOpen(false)
+                                            combobox.closeDropdown()
+                                        }}
+                                    />
+                                </form>
+                            </Combobox.EventsTarget>
+                            <Combobox.Options className={classes.searchInputMobileOptions}>
+                                <ScrollArea.Autosize h="calc(100dvh - 58px)" ref={searchInputRef} type="scroll"
+                                                     className={classes.searchInputMobileScrollArea}>
+                                    {empty && <Combobox.Empty>No matching courses for "{value}"</Combobox.Empty>}
+                                    {options}
+                                </ScrollArea.Autosize>
+                            </Combobox.Options>
+                            <Combobox.Footer>
+                                <Text fz="xs" c="dimmed">
+                                    TUM-RATING © 2024
+                                </Text>
+                            </Combobox.Footer>
+                        </Combobox>
+                    )
+                }
+
+
+            </>
+        )
+    }
+
+    if (smallerMode === undefined) return <Loader size="xs"/>;
+
     return (
         <Combobox
             onOptionSubmit={(optionValue) => {
-                setValue(optionValue);
+                navigate('/courses/' + optionValue);
                 combobox.closeDropdown();
             }}
             offset={0}
--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -16,13 +16,16 @@ import {
 } from '@mantine/core';
 import {useDisclosure, useHotkeys} from '@mantine/hooks';
 import logo from '@/assets/img/logo.png';
-import {IconMoonStars, IconSearch, IconSun} from '@tabler/icons-react';
+import {IconMoonStars, IconSun} from '@tabler/icons-react';
 import {useUser} from '@/auth/useUser';
 import {UserButton} from '@/components/UserButton';
 import {useSignOut} from '@/auth/useSignOut';
 import {getPath, Paths} from '@/routes/paths.ts';
 import {useNavigate} from 'react-router-dom';
 import {SearchInputDesktop} from "@/components/Search";
+import {isMobileOnly} from "react-device-detect";
+import {useMediaQuery} from "@mantine/hooks";
+
 
 const HEADER_HEIGHT = 54;
 const MAX_SITE_WIDTH = 1320;
@@ -32,8 +35,8 @@ export const MainLayout = ({children}: PropsWithChildren) => {
     const navigate = useNavigate();
     const [mobileOpened, {toggle: toggleMobile}] = useDisclosure();
     const {colorScheme, toggleColorScheme} = useMantineColorScheme();
+    const smallerMode = useMediaQuery('(max-width: 48em)');
     const signOut = useSignOut();
-
     useHotkeys([['/', () => navigate(getPath(Paths.spotlight))]]);
     return (
         <AppShell header={{height: HEADER_HEIGHT}} padding="md">
@@ -43,10 +46,11 @@ export const MainLayout = ({children}: PropsWithChildren) => {
                         <Image data-test="app-logo" fit="contain" height={28} width={129} src={logo}
                                alt="tum rating logo"/>
                     </Anchor>
-                    <Flex maw={580} style={{flexGrow: 1}}>
-                        <SearchInputDesktop/>
-                    </Flex>
-
+                    {!isMobileOnly && !smallerMode && (
+                        <Flex maw={580} style={{flexGrow: 1}}>
+                            <SearchInputDesktop/>
+                        </Flex>
+                    )}
                     <Flex gap={20}>
                         {!user ? (
                             <>
@@ -73,16 +77,13 @@ export const MainLayout = ({children}: PropsWithChildren) => {
                         </ActionIcon>
                     </Flex>
                 </Flex>
-                <Group hiddenFrom="sm" h="100%" px="md" justify="space-between">
+                <Group hiddenFrom="sm" h="100%" px="md" justify="space-between" pos="relative">
                     <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm"/>
                     <Anchor href="/">
                         <Image data-test="app-logo" fit="contain" height={28} width={129} src={logo}
                                alt="tum rating logo"/>
                     </Anchor>
-                    <ActionIcon variant="outline" data-testid="cypress-global-search"
-                                onClick={() => navigate(getPath(Paths.spotlight))}>
-                        <IconSearch size="1.1rem"/>
-                    </ActionIcon>
+                    <SearchInputDesktop/>
                 </Group>
             </AppShell.Header>
             <AppShell.Main p={0} m={0} style={{background: "var(--primary-layout-gradient)"}}>
