8deeb9aa484e37dc095037695f520b155931c0cc
Jędrzej Rzankowski <j.rzankowski@gmail.com>
2024-03-01 22:57:04 +0100

    fix(frontend): fix useEffect throttling in ModalsHashController.tsx

--- frontend/src/components/Modals/ModalsHashController.tsx
+++ frontend/src/components/Modals/ModalsHashController.tsx
@@ -1,22 +1,22 @@
-import { closeAllModals, useModals } from '@mantine/modals';
-import { PropsWithChildren, useEffect } from 'react';
-import { isMobile } from 'react-device-detect';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import {closeAllModals, useModals} from '@mantine/modals';
+import {PropsWithChildren, useEffect, useMemo} from 'react';
+import {isMobile} from 'react-device-detect';
+import {useLocation, useNavigate, useParams} from 'react-router-dom';
 
-import { openAddCourseModal } from '@/components/Modals/AddCourseModal';
-import { openAddUserReviewModal } from '@/components/Modals/AddUserReview';
-import { openEditUserReviewModal } from '@/components/Modals/EditUserReview';
-import { openRecoveryModal } from '@/components/Modals/RecoveryModal';
-import { openSignInModal } from '@/components/Modals/SignInModal';
-import { openSignUpModal } from '@/components/Modals/SignUpModal';
+import {openAddCourseModal} from '@/components/Modals/AddCourseModal';
+import {openAddUserReviewModal} from '@/components/Modals/AddUserReview';
+import {openEditUserReviewModal} from '@/components/Modals/EditUserReview';
+import {openRecoveryModal} from '@/components/Modals/RecoveryModal';
+import {openSignInModal} from '@/components/Modals/SignInModal';
+import {openSignUpModal} from '@/components/Modals/SignUpModal';
 
 
 export const ModalsHashController = (_: PropsWithChildren) => {
     const location = useLocation();
     const navigate = useNavigate();
-    const { id } = useParams();
+    const {id} = useParams();
     let modalsContext = useModals();
-    const modalSharedParams = {
+    const modalSharedParams = useMemo(() => ({
         onClose: () => {
             closeAllModals();
             navigate('#');
@@ -26,21 +26,21 @@ export const ModalsHashController = (_: PropsWithChildren) => {
             backgroundOpacity: 0.55,
             blur: 3,
         },
-    };
-    const modals = {
-        'sign-in': { component: openSignInModal, params: { ...modalSharedParams } },
-        'sign-up': { component: openSignUpModal, params: { ...modalSharedParams } },
-        'forgot-password': { component: openRecoveryModal, params: { ...modalSharedParams } },
-        'add-course': { component: openAddCourseModal, params: { ...modalSharedParams } },
+    }), [navigate]);
+    const modals = useMemo(() => ({
+        'sign-in': {component: openSignInModal, params: {...modalSharedParams}},
+        'sign-up': {component: openSignUpModal, params: {...modalSharedParams}},
+        'forgot-password': {component: openRecoveryModal, params: {...modalSharedParams}},
+        'add-course': {component: openAddCourseModal, params: {...modalSharedParams}},
         'add-user-review': {
             component: openAddUserReviewModal,
-            params: { ...modalSharedParams, innerProps: { courseId: id } },
+            params: {...modalSharedParams, innerProps: {courseId: id}},
         },
         'edit-user-review': {
             component: openEditUserReviewModal,
-            params: { ...modalSharedParams, innerProps: { courseId: id } },
+            params: {...modalSharedParams, innerProps: {courseId: id}},
         }
-    };
+    }), [modalSharedParams, id]);
 
     useEffect(() => {
         if (location.hash.includes('#modal=')) {
@@ -54,6 +54,6 @@ export const ModalsHashController = (_: PropsWithChildren) => {
                 closeAllModals();
             }
         }
-    }, [location, modals, modalsContext.modals.length]);
+    }, [location]);
     return null;
 };
