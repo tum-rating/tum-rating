0d1a450c7ab59e2479ec401ee0b188a7208c1d24
JRzankowski <j.rzankowski@gmail.com>
2024-04-05 14:49:22 +0200

    feat(frontend): Admin-panel authorization, add PageAdminNotFound error page

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -4,7 +4,9 @@ import {Outlet, useNavigate} from "react-router-dom";
 
 import classes from './AdminLayout.module.css';
 
+import {useCoursesProposals} from "@/admin/useCoursesProposals.ts";
 import {useUser} from "@/auth/useUser.tsx";
+import {PageAdminNotFound} from "@/pages/PageNotFound";
 import {getPath, Paths} from "@/routes/paths.ts";
 
 
@@ -16,54 +18,63 @@ const adminTabs = [
 
 export function AdminLayout() {
     const [opened, {toggle}] = useDisclosure();
+    const {error, isFetched, isFetchedAfterMount} = useCoursesProposals();
     const {user} = useUser();
     const navigate = useNavigate();
-    if(!user){
-        navigate("/");
-        return null;
-    }else return (
-        <AppShell
-            header={{height: 60}}
-            navbar={{width: 300, breakpoint: 'sm', collapsed: {desktop: true, mobile: !opened}}}
-            padding="md"
-        >
-            <AppShell.Header>
-                <Group h="100%" px="md">
-                    <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm"/>
-                    <Group justify="space-between" style={{flex: 1}}>
-                        <Flex align="center">
-                            <Text
-                                size="lg"
-                                fw={900}
-                                variant="gradient"
-                                gradient={{from: 'grape', to: 'violet', deg: 90}}
-                            >
-                                TUM-RATING ADMIN
-                            </Text>
-                        </Flex>
-                        <Group ml="xl" gap={0} visibleFrom="sm">
-                            <Group gap={0}>
-                                {adminTabs.map((tab, index) => (
-                                    <UnstyledButton key={index} className={classes.control} onClick={() => navigate(tab.link)}>
-                                        {tab.label}
-                                    </UnstyledButton>
-                                ))}
+    if (!user || isFetchedAfterMount) {
+        return <PageAdminNotFound/>;
+    } else if (!error && isFetched) {
+        return (
+            <AppShell
+                header={{height: 60}}
+                navbar={{width: 300, breakpoint: 'sm', collapsed: {desktop: true, mobile: !opened}}}
+                padding="md"
+            >
+                <AppShell.Header>
+                    <Group h="100%" px="md">
+                        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm"/>
+                        <Group justify="space-between" style={{flex: 1}}>
+                            <Flex align="center">
+                                <Text
+                                    size="lg"
+                                    fw={900}
+                                    variant="gradient"
+                                    gradient={{from: 'grape', to: 'violet', deg: 90}}
+                                >
+                                    TUM-RATING ADMIN
+                                </Text>
+                            </Flex>lkk
+                            <Group ml="xl" gap={0} visibleFrom="sm">
+                                <Group gap={0}>
+                                    {adminTabs.map((tab, index) => (
+                                        <UnstyledButton key={index} className={classes.control}
+                                                        onClick={() => navigate(tab.link)}>
+                                            {tab.label}
+                                        </UnstyledButton>
+                                    ))}
+                                </Group>
                             </Group>
-
                         </Group>
                     </Group>
-                </Group>
-            </AppShell.Header>
-            <AppShell.Navbar py="md" px={4}>
-                {adminTabs.map((tab, index) => (
-                    <UnstyledButton key={index} className={classes.control} onClick={() => navigate(tab.link)}>
-                        {tab.label}
-                    </UnstyledButton>
-                ))}
-            </AppShell.Navbar>
-            <AppShell.Main style={{background: "var(--primary-light-gradient)"}}>
-                <Outlet/>
-            </AppShell.Main>
-        </AppShell>
-    );
+                </AppShell.Header>
+                <AppShell.Navbar py="md" px={4}>
+                    {adminTabs.map((tab, index) => (
+                        <UnstyledButton key={index} className={classes.control} onClick={() => navigate(tab.link)}>
+                            {tab.label}
+                        </UnstyledButton>
+                    ))}
+                </AppShell.Navbar>
+                <AppShell.Main style={{background: "var(--primary-light-gradient)"}}>
+                    {
+                        user ? (
+                            <Outlet/>
+                        ) : null
+                    }
+                </AppShell.Main>
+
+
+            </AppShell>
+        );
+    }
+    return null;
 }
---
+++ frontend/src/pages/PageNotFound/PageAdminNotFound.tsx
@@ -0,0 +1,23 @@
+import {Button, Center, Container, Flex, Text, Image} from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+
+import classes from './PageNotFound.module.css';
+
+import wh from '@/assets/img/wh5.png';
+
+export function PageAdminNotFound() {
+    const navigate = useNavigate();
+    return (
+        <Container className={classes.root}>
+            <Center h="100%">
+                <Flex justify="center" align="center" direction="column" gap="xs">
+                    <Image className={classes.whereAreYouGoing} src={wh}/>
+                    <Text c="dimmed" fw="500">
+                        Where are you going? 🤔
+                    </Text>
+                    <Button onClick={() => navigate('/')}>Go back to home</Button>
+                </Flex>
+            </Center>
+        </Container>
+    );
+}
