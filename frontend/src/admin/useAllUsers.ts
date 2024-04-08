5ad967c9d04078c576b620904efffd12890959d5
Jędrzej Rzankowski <j.rzankowski@gmail.com>
2024-04-09 00:43:42 +0200

    fix(frontend): fix user auth flow

--- frontend/src/admin/useAllUsers.ts
+++ frontend/src/admin/useAllUsers.ts
@@ -1,20 +1,26 @@
-import { useQuery } from '@tanstack/react-query';
+import {useQuery} from '@tanstack/react-query';
 
-import { endpoints } from '@/api';
-import { QUERY_KEY } from '@/constants/queryKeys.ts';
-import { ResponseError } from '@/utils/Errors/ResponseError.ts';
+import {endpoints} from '@/api';
+import * as userLocalStorage from "@/auth/user.localstore.ts";
+import {QUERY_KEY} from '@/constants/queryKeys.ts';
+import {ResponseError} from '@/utils/Errors/ResponseError.ts';
 
-async function getAllUsers(): Promise<any[] | null> {
-    const response = await fetch(endpoints.getAllUsers);
+async function getAllUsers(token: string): Promise<any[] | null> {
+    const response = await fetch(endpoints.getAllUsers,{
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
     if (!response.ok) throw new ResponseError('Failed on get reviews request', response);
     const data = await response.json();
     return await data;
 }
 
 export function useAllUsers() {
+    const token = userLocalStorage.getUser();
     return useQuery({
         queryKey: [QUERY_KEY.all_users],
-        queryFn: async () => getAllUsers(),
+        queryFn: async () => getAllUsers(token),
         refetchIntervalInBackground: true,
         refetchInterval: 1000 * 60 * 5, // 5 minutes
     });
