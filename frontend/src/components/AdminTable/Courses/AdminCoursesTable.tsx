d1855304ff73221a361c17238e9d60a2865f652e
JRzankowski <j.rzankowski@gmail.com>
2024-04-19 15:47:56 +0200

    wip(frontend): All-courses admin table

--- frontend/src/components/AdminTable/Courses/AdminCoursesTable.tsx
+++ frontend/src/components/AdminTable/Courses/AdminCoursesTable.tsx
@@ -1,85 +1,75 @@
-import {ActionIcon, Code, Flex, rem, Text, Box} from "@mantine/core";
-import {useViewportSize} from "@mantine/hooks";
-import {IconRefresh} from "@tabler/icons-react";
+import {Badge, Box, Button, Flex, Group, Text} from "@mantine/core";
+import {IconFilterX, IconRefresh} from "@tabler/icons-react";
 import {DataTable, DataTableProps} from "mantine-datatable";
-import {useEffect, useState} from "react";
 
-import {columns} from "./columns.tsx"
+import {useCoursesColumns} from "./useCoursesColumns.tsx"
+import classes from "../Shared/styles/TableStyles.module.css"
 
-import {Course} from "@/courses/types.ts";
+import {CourseExpansion} from "@/components/AdminTable/Courses/CourseExpansion.tsx";
 import {usePaginatedCourses} from "@/courses/usePaginatedCourses.tsx";
 
-
 const AdminCoursesTable = () => {
-    // const {data, isFetching, refetch} = useCoursesProposals();
-    const { data, fetchNextPage, isFetching,refetch } = usePaginatedCourses();
-    const { height } = useViewportSize();
-    const [records, setRecords] = useState<Course[]>([]);
+    const {fetchNextPage, isFetching, refetch} = usePaginatedCourses();
+    const {data: courses, columns, resetFilters, isAnyFilterActive} = useCoursesColumns();
 
-    useEffect(() => {
-        if (data) {
-            const newRecords = data.pages.map((v) => v.courses.map((el) => el)).flat();
-            setRecords([...newRecords]);
-        }
-    }, [data]);
 
     const loadMoreRecords = () => {
-        fetchNextPage().then(() => {});
+        fetchNextPage().then(() => {
+        });
     };
 
     const rowExpansion: DataTableProps<any>['rowExpansion'] = {
         allowMultiple: true,
-        content: ({record}) => {
-            return (
-                <Flex direction="column" p="xs" pl={rem(50)}>
-                    {Object.entries(record).map(([key, value]) => {
-                        return (
-                            <Flex key={key} align="center">
-                                <Code>{key} :</Code>
-                                <Code>{JSON.stringify(value)}</Code>
-                            </Flex>
-                        );
-                    })}
-                </Flex>
-            )
+        collapseProps: {
+            transitionDuration: 0,
+            animateOpacity: false,
         },
+        content: ({record}) => <CourseExpansion course={record} editing={false}/>
     };
 
     return (
-        <Box h={height - 150}>
-            <Text fw={600}>
-                Courses
-            </Text>
-            <Flex justify="space-between" align="center" h={50} pr="xs">
-                <Flex gap="xs">
-                    <Text size="sm" fw={500}>
-                        All Proposals:
-                    </Text>
-                    <Text size="sm" fw={800}>
-                        {records.length}
+        <Box h="calc(100vh-110px)">
+            <Flex justify="space-between" align="center" h={50} px="xs" bg="gray.1">
+                <Flex gap="6" align="center">
+                    <Badge radius="sm" fw={800} c="white" px={6}>
+                        {courses.length}
+                    </Badge>
+                    <Text fw={600}>
+                        Proposals
                     </Text>
                 </Flex>
-                <ActionIcon
-                    variant="light"
-                    size="xs"
-                    onClick={() => refetch()}>
-                    <IconRefresh size={16}/>
-                </ActionIcon>
+                <Group>
+                    {isAnyFilterActive && (
+                        <Button
+                            variant="light"
+                            size="xs"
+                            rightSection={<IconFilterX size={16}/>}
+                            onClick={() => resetFilters()}>
+                            Reset filters
+                        </Button>
+                    )}
+                    <Button
+                        variant="light"
+                        size="xs"
+                        rightSection={<IconRefresh size={16}/>}
+                        onClick={() => refetch()}>
+                        Refresh
+                    </Button>
+                </Group>
             </Flex>
             <DataTable
+                height={100}
                 withTableBorder
-                height="100%"
-                borderRadius="sm"
                 withColumnBorders
                 idAccessor='_id'
                 striped
-                verticalAlign="top"
                 pinLastColumn
-                columns={columns}
                 fetching={isFetching}
-                records={records}
+                records={courses}
+                className={classes.table}
                 onScrollToBottom={loadMoreRecords}
                 rowExpansion={rowExpansion}
+                columns={columns}
             />
         </Box>
     );
