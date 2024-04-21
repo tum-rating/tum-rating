636c0d69c864dde069f9e733365dc7379cbfb072
Waldemar Zieliński <waldemar99zielinski@gmail.com>
2024-04-21 16:59:49 +0200

    test(backend): refactored course proposal

--- backend/test/course-proposal/GetAllCourseProposals.test.ts
+++ backend/test/course-proposal/GetAllCourseProposals.test.ts
@@ -4,6 +4,7 @@ import * as supertest from 'supertest';
 import { courseProposalUrl } from '@tum-rating/backend/test/utils/api-client/course-proposal';
 import { connectMongo, signInRequestMock, signInAdminRequestMock } from '@tum-rating/backend/test/utils';
 import { createCourseProposalMockRequest } from '@tum-rating/backend/test/utils/api-client/course-proposal';
+import exp from 'constants';
 
 beforeAll(async () => {
     await connectMongo();
@@ -13,42 +14,40 @@ afterAll(async () => {
     mongoose.disconnect();
 });
 
-describe('Get Course Proposal', () => {
-    it('should get course proposal', async () => {
+describe('Get All Course Proposal', () => {
+    it('should get all course proposals', async () => {
         const signInResponse = await signInRequestMock();
         const singInAdminResponse = await signInAdminRequestMock();
 
-        const createdCourseProposal = await createCourseProposalMockRequest(signInResponse.token);
+        await createCourseProposalMockRequest(signInResponse.token);
 
         return supertest(courseProposalUrl)
-            .get('/' + createdCourseProposal.id)
+            .get('/') 
             .set('Authorization', 'Bearer ' + singInAdminResponse.token)
             .expect(200)
             .expect((response: supertest.Response) => {
-                expect(response.body.id).toEqual(createdCourseProposal.id);
-                expect(response.body.url).toEqual(createdCourseProposal.url);
-                expect(response.body.userId).toEqual(signInResponse.user.id);
-                expect(response.body.createdAt).toBeDefined();
+                expect(response.body.courseProposals).toBeDefined();
+                expect(response.body.courseProposals.length).toBeGreaterThan(0);
+                expect(response.body.courseProposals[0].id).toBeDefined();
+                expect(response.body.courseProposals[0].url).toBeDefined();
+                expect(response.body.courseProposals[0].userId).toBeDefined();
+                expect(response.body.courseProposals[0].createdAt).toBeDefined();
             });
     });
 
     it('should fail without auth token', async () => {
         const signInResponse = await signInRequestMock();
 
-        const createdCourseProposal = await createCourseProposalMockRequest(signInResponse.token);
-
         return supertest(courseProposalUrl)
-            .get('/' + createdCourseProposal.id)
+            .get('/')
             .expect(401);
     });
 
     it('should fail without admin auth token', async () => {
         const signInResponse = await signInRequestMock();
 
-        const createdCourseProposal = await createCourseProposalMockRequest(signInResponse.token);
-
         return supertest(courseProposalUrl)
-            .get('/' + createdCourseProposal.id)
+            .get('/')
             .set('Authorization', 'Bearer ' + signInResponse.token)
             .expect(403);
     });
