8bc5eb6c1bfa0987c09a971b95775603fea60bbb
Jędrzej Rzankowski <j.rzankowski@gmail.com>
2024-04-23 00:04:27 +0200

    fix: react-query type errors

--- frontend/src/api/useQueryWithAuth.tsx
+++ frontend/src/api/useQueryWithAuth.tsx
@@ -1,21 +1,29 @@
-
-import {UseQueryResult, UseQueryOptions, useQuery} from '@tanstack/react-query';
+import {DefaultError, QueryKey} from "@tanstack/query-core";
+import {useQuery, UseQueryOptions, UseQueryResult} from '@tanstack/react-query';
 import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 
 import {handleAuthErrors} from "@/api/handleErrors.tsx";
 import {useSignOut} from "@/auth/useSignOut.tsx";
 
-export function useQueryWithAuth<TQueryFnData = unknown, TError = unknown, TData = void>(options: UseQueryOptions<TQueryFnData, TError, TData>): UseQueryResult<TData, TError> {
+
+export function useQueryWithAuth<
+    TQueryFnData = unknown,
+    TError = DefaultError,
+    TData = TQueryFnData,
+    TQueryKey extends QueryKey = QueryKey,
+>(
+    options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
+): UseQueryResult<TData, TError> {
     const signOut = useSignOut();
     const navigate = useNavigate()
-    const query = useQuery<TQueryFnData, TError, TData>({
+    const query = useQuery({
         ...options,
     });
-    const { isError, error } = query;
+    const {isError, error} = query;
     useEffect(() => {
         if (isError) {
-            handleAuthErrors({ error, signOut,navigate });
+            handleAuthErrors({error, signOut, navigate});
         }
     }, [isError]);
 
