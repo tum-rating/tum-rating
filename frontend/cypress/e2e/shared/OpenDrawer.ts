1f4dfe8bf5e548292bda8687c8a51d7913701656
JRzankowski <j.rzankowski@gmail.com>
2024-05-23 10:46:46 +0200

    chore: cleanup, bump packages

--- frontend/cypress/e2e/shared/OpenDrawer.ts
+++
@@ -1,5 +0,0 @@
-export const openDrawer = () => {
-    cy.get('[data-testid="cypress-drawer"]').should('not.be.visible');
-    cy.get('[data-testid="cypress-burger"]').click();
-    cy.get('[data-testid="cypress-drawer"]').should('be.exist');
-};
