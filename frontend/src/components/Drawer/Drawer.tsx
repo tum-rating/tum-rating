cfe384c9e8e1508c04a240a476057213c0d71456
Jędrzej Rzankowski <j.rzankowski@gmail.com>
2024-06-26 00:21:47 +0200

    refactor(front): swipe logic

--- frontend/src/components/Drawer/Drawer.tsx
+++ frontend/src/components/Drawer/Drawer.tsx
@@ -1,9 +1,29 @@
-import { ReactNode, useEffect, useState } from 'react';
+import {ReactNode, useCallback, useEffect, useRef, useState} from 'react';
+import {isMobile} from "react-device-detect";
 
-import { CLOSED_DRAWER_ANIMATION_NAME, CLOSED_DRAWER_X, DRAWER_CONTENT_WIDTH, DRAWER_LEFT_OVERFLOW_WIDTH, DRAWER_WIDTH, OPENED_DRAWER_ANIMATION_NAME, OPENED_DRAWER_X } from './constans.ts';
-import { closedDrawerAnimation, openedDrawerAnimation } from './utils.ts';
+import {
+    DRAG_THRESHOLD,
+    SWIPEABLE_AREA,
+    DRAWER_WIDTH
+} from './constans.ts';
+import {getPointerCoordinates} from './utils.ts';
 
-import { Keyframe } from '@/components/Keyframe';
+
+
+export interface SwipeState {
+    swiping: boolean
+    alphaX: number
+    count: number
+}
+
+const initialState: SwipeState = { swiping: false, alphaX: 0, count: 0 }
+
+const isEqual = (prev: SwipeState, next: SwipeState): boolean => (
+    prev.swiping === next.swiping &&
+    prev.count === next.count &&
+    prev.alphaX === next.alphaX
+)
+
 
 interface DrawerProps {
     open: boolean;
@@ -12,131 +32,135 @@ interface DrawerProps {
 }
 
 const Drawer = (props: DrawerProps) => {
-    const { open, children } = props;
+    const {open, children, toggleFn} = props;
+    const [state, setState] = useState(initialState)
     const [isOpen, setIsOpen] = useState(open);
+    const [drawerX, setDrawerX] = useState(0)
 
-    const [startX, setStartX] = useState(0);
-    const [endX, setEndX] = useState(0);
+    const startingPointRef = useRef<number>(-1)
+    const isDraggingRef = useRef(false)
 
-    const [drawerStartX, setDrawerStartX] = useState(CLOSED_DRAWER_X);
 
-    const [drawerX, setDrawerX] = useState(CLOSED_DRAWER_X);
-    const [drag, setDrag] = useState(false);
+    const startSwipe = (e: MouseEvent | TouchEvent) => {
+        e.preventDefault()
+        e.stopPropagation()
+        const clientX = getPointerCoordinates(e);
+        if (!isOpen && clientX > SWIPEABLE_AREA) return;
+        startingPointRef.current = clientX;
+    }
 
-    const onTouchStart = (e: TouchEvent) => {
-        if (!isOpen) {
-            if (drawerX <= e.touches[0].clientX && e.touches[0].clientX <= 10 && !drag) {
-                setStartX(e.touches[0].clientX);
-                setEndX(e.touches[0].clientX);
-                setDrawerStartX(drawerStartX + (e.touches[0].clientX - startX));
-                setDrag(true);
+    const continueSwipe = (e: MouseEvent | TouchEvent) => {
+        const clientX = getPointerCoordinates(e)
+        if (isDraggingRef.current || (startingPointRef.current !== -1)) {
+            const alpha = startingPointRef.current - clientX
+
+            if (Math.abs(alpha) > DRAG_THRESHOLD) {
+                isDraggingRef.current = true
+
+                const nextState: SwipeState = {
+                    alphaX: alpha,
+                    count: state.count,
+                    swiping: true,
+                }
+
+                if (!isEqual(nextState, state)) {
+                    setState(nextState)
+                }
             }
+
+        }
+    }
+
+    const endSwipe = (event: MouseEvent | TouchEvent) => {
+        if (isDraggingRef.current) {
+                event.preventDefault()
+                event.stopPropagation()
+
+
+            setState((prevState) => ({
+                ...prevState,
+                swiping: false,
+                count: state.count + 1
+            }))
+        }
+
+        startingPointRef.current = -1
+        isDraggingRef.current = false
+    }
+
+
+    const onTouchStart = useCallback((e: TouchEvent) => {
+        startSwipe(e);
+    }, [startSwipe]);
+
+    const onTouchMove = useCallback((e: TouchEvent) => {
+        continueSwipe(e);
+    }, [continueSwipe]);
+
+    const onTouchEnd = useCallback((e: TouchEvent) => {
+        endSwipe(e);
+    }, [endSwipe]);
+
+    const onMouseDown = useCallback((e: MouseEvent) => {
+        startSwipe(e);
+    }, [startSwipe]);
+
+    const onMouseMove = useCallback((e: MouseEvent) => {
+        continueSwipe(e);
+    }, [continueSwipe]);
+
+    const onMouseUp = useCallback((e: MouseEvent) => {
+        endSwipe(e);
+    }, [endSwipe]);
+
+
+    useEffect(() => {
+        if (isMobile) {
+            document.addEventListener('touchstart', onTouchStart);
+            document.addEventListener('touchmove', onTouchMove);
+            document.addEventListener('touchend', onTouchEnd);
         } else {
-            if(drag){
-                if (e.touches[0].clientX <= DRAWER_CONTENT_WIDTH) {
-                    setStartX(e.touches[0].clientX);
-                    setEndX(e.touches[0].clientX);
-                    setDrawerStartX(drawerStartX + (e.touches[0].clientX - startX));
-                }
-            }
+            document.addEventListener('mousedown', onMouseDown);
+            document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('mouseup', onMouseUp);
         }
-    };
-
-    const onTouchMove = (e: TouchEvent) => {
-            if(drag){
-                if (e.touches[0].clientX >= DRAWER_WIDTH) {
-                } else {
-                    const x = drawerStartX + (e.touches[0].clientX - startX);
-                    setDrawerX(x);
-                }
-            }
-    };
-
-    const onTouchEnd = (e: TouchEvent) => {
-        if (drag) {
-            setDrag(false);
-
-            if (e.changedTouches[0].clientX >= DRAWER_CONTENT_WIDTH / 2) {
-                setDrawerX(OPENED_DRAWER_X);
-                if (e.changedTouches[0].clientX >= DRAWER_WIDTH) {
-                    setEndX(DRAWER_CONTENT_WIDTH);
-                } else {
-                    setEndX(drawerStartX + (e.changedTouches[0].clientX - startX));
-                }
-                setIsOpen(true);
-            } else {
-                setDrawerX(CLOSED_DRAWER_X);
-                setEndX(drawerStartX + (e.changedTouches[0].clientX - startX));
-                setIsOpen(false);
-            }
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener('touchmove', onTouchMove);
-        window.addEventListener('touchend', onTouchEnd);
-        window.addEventListener('touchstart', onTouchStart);
-
         return () => {
-            window.removeEventListener('touchmove', onTouchMove);
-            window.removeEventListener('touchend', onTouchEnd);
-            window.addEventListener('touchstart', onTouchStart);
-        };
-    }, [drag, startX, drawerStartX]);
-
-    const toggleDrawer = () => {
-        setIsOpen(!isOpen);
-    };
+            if (isMobile) {
+                document.removeEventListener('touchstart', onTouchStart);
+                document.removeEventListener('touchmove', onTouchMove);
+                document.removeEventListener('touchend', onTouchEnd);
+            } else {
+                document.removeEventListener('mousedown', onMouseDown);
+                document.removeEventListener('mousemove', onMouseMove);
+                document.removeEventListener('mouseup', onMouseUp);
+            }
+        }
+    }, [document]);
 
     useEffect(() => {
-        setIsOpen(open);
-    }, [open]);
+        console.log(state)
+        setDrawerX(state.alphaX)
+    }, [state]);
 
     return (
         <>
-            {!drag && <Keyframe name={isOpen ? OPENED_DRAWER_ANIMATION_NAME : CLOSED_DRAWER_ANIMATION_NAME} animationProps={isOpen ? openedDrawerAnimation({ x: endX }) : closedDrawerAnimation({ x: endX })} />}
-            <div
-                style={{
-                    transform: `${drag ? `translateX(${drawerX}px)` : 'none'}`,
-                    animation: `${!drag ? (isOpen ? OPENED_DRAWER_ANIMATION_NAME : CLOSED_DRAWER_ANIMATION_NAME) : ''} .4s both`,
-                    position: 'fixed',
-                    top: 0,
-                    left: 0,
-                    height: '100%',
-                    width: DRAWER_WIDTH + 'px',
-                    background: 'orange',
-                    zIndex: 9999,
-                    display: 'flex',
-                }}
-            >
-                <div style={{ width: `${DRAWER_LEFT_OVERFLOW_WIDTH}px`, background: 'red' }}></div>
-                <div
-                    style={{
-                        width: `${DRAWER_CONTENT_WIDTH}px`,
-                        background: `green`,
-                    }}
-                >
-                    <div style={{ width: '200px', background: 'gray' }}>
-                        <button onClick={toggleDrawer}>Toggle Drawer</button>
-                        {children}
-                    </div>
+            <div style={{
+                transform: `translateX(${-drawerX}px)`,
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                background: "gray",
+                width: DRAWER_WIDTH,
+                height: '100%',
+                zIndex: 1000,
+            }}>
+                <div className="content">
+                    {children}
                 </div>
             </div>
-            <div
-                className="backdrop"
-                style={{
-                    position: 'fixed',
-                    top: 0,
-                    left: 0,
-                    height: '100%',
-                    width: '100%',
-                    background: 'rgba(0,0,0,0.5)',
-                    zIndex: 9998,
-                    display: drag || isOpen ? 'block' : 'none',
-                }}
-            ></div>
+
         </>
     );
 };
 
-export { Drawer };
+export {Drawer};
