2ef7c828b4088cc68251770f7c79db2445974e0c
Waldemar Zieliński <waldemar99zielinski@gmail.com>
2024-06-27 12:56:58 +0200

    improv(backend): database docs enforce creation and indexes

--- backend/src/database/documents/review.ts
+++ backend/src/database/documents/review.ts
@@ -3,7 +3,10 @@ import { Document, Schema as MongooseSchema } from 'mongoose';
 
 import { User } from './user';
 
-@Schema()
+@Schema({
+    autoCreate: true, 
+    autoIndex: true
+})
 export class Review {
     @Prop({
         required: true,
