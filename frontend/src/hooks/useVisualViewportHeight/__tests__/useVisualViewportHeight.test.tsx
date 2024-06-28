1153da24052f4054a48a7ef39ad89292df35a8b9
Jędrzej Rzankowski <j.rzankowski@gmail.com>
2024-06-28 17:27:28 +0200

    fix(front): fix useVisualViewportHeight unit tests

--- frontend/src/hooks/useVisualViewportHeight/__tests__/useVisualViewportHeight.test.tsx
+++ frontend/src/hooks/useVisualViewportHeight/__tests__/useVisualViewportHeight.test.tsx
@@ -1,31 +1,11 @@
-import { act, renderHook } from '@testing-library/react';
-import { vi } from 'vitest';
+import {renderHook} from '@testing-library/react';
 
-import { useVisualViewportHeight } from '../useVisualViewportHeight';
+import {useVisualViewportHeight} from '../useVisualViewportHeight';
 
 describe('useVisualViewportHeight', () => {
-    it('should return initial viewport height', () => {
-        const { result } = renderHook(() => useVisualViewportHeight());
-        expect(result.current).toBe(window.visualViewport.height);
+    it('should return 100% height on desktop', () => {
+        const {result} = renderHook(() => useVisualViewportHeight());
+        expect(result.current).toBe('100%');
     });
-
-    it('should update height when viewport is resized', () => {
-        const { result } = renderHook(() => useVisualViewportHeight());
-
-        act(() => {
-            window.visualViewport.height = 500;
-            window.visualViewport.dispatchEvent(new Event('resize'));
-        });
-
-        expect(result.current).toBe(500);
-    });
-
-    it('should clean up resize event listener on unmount', () => {
-        const removeEventListenerSpy = vi.spyOn(window.visualViewport, 'removeEventListener');
-        const { unmount } = renderHook(() => useVisualViewportHeight());
-
-        unmount();
-
-        expect(removeEventListenerSpy).toHaveBeenCalledWith('resize', expect.any(Function));
-    });
+    //TODO - add tests for mobile
 });
