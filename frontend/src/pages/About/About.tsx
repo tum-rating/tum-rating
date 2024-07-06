91d66b4ddd024012706ce4dc2444d1fef4034e1c
Jędrzej Rzankowski <j.rzankowski@gmail.com>
2024-07-07 00:58:24 +0200

    feat(front): add PageWrapper component

---
+++ frontend/src/pages/About/About.tsx
@@ -0,0 +1,29 @@
+import {Flex, Text, Title} from "@mantine/core";
+import {useEffect} from "react";
+
+import {PageWrapper} from "@/components/PageWrapper";
+
+
+export const About = () => {
+
+    useEffect(() => {
+        const children = document.querySelectorAll('.children-animation > *');
+        children.forEach((child: Element, index: number) => {
+            (child as HTMLElement).style.animationDelay = `${0.025 * (index + 1)}s`;
+        });
+    }, []);
+
+    return (
+        <PageWrapper autoHeight>
+            <Flex direction="column" align="center" justify="center" mt="xl" gap="xs" p="xl"
+                  className="children-animation">
+                <Title>
+                    About Us
+                </Title>
+                <Text ta="center">
+                    We just want to make website with TUM University courses rating.
+                </Text>
+            </Flex>
+        </PageWrapper>
+    );
+};
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,6 +1,10 @@
-export { Home } from './Home/Home.tsx';
-export { Course } from './Course';
-export { ErrorBoundary } from './PageNotFound';
-export { Activation } from './Activation/Activation.tsx';
-export { Recovery } from './Recovery/Recovery.tsx';
-export { Admin } from './Admin/Admin.tsx';
+export {Home} from './Home/Home.tsx';
+export {Course} from './Course';
+export {ErrorBoundary} from './PageNotFound';
+export {Activation} from './Activation/Activation.tsx';
+export {Recovery} from './Recovery/Recovery.tsx';
+export {Admin} from './Admin/Admin.tsx';
+export {About} from "./About"
+export {Contact} from "./Contact"
+export {PrivacyPolicy} from "./PrivacyPolicy"
+
