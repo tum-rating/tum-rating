ffc46e1eda1514ee1c06a4b02b67713f6f370d41
Jędrzej Rzankowski <j.rzankowski@gmail.com>
2024-07-09 19:47:57 +0200

    improv(front): FloatingMenuTelegramButton mobile display

--- frontend/src/components/FloatingMenu/FloatingMenuTelegramButton/FloatingMenuTelegramButton.tsx
+++ frontend/src/components/FloatingMenu/FloatingMenuTelegramButton/FloatingMenuTelegramButton.tsx
@@ -1,6 +1,7 @@
 import { Progress, Text, UnstyledButton, Box } from '@mantine/core';
 import clsx from 'clsx';
 import { useEffect, useState, useRef } from 'react';
+import { isMobile} from "react-device-detect";
 
 import classes from './FloatingMenuTelegramButton.module.css';
 import { TelegramIcon } from './TelegramIcon.tsx';
@@ -47,11 +48,11 @@ const FloatingMenuTelegramButton = () => {
             onMouseLeave={() => setStatus('inactive')}
             className={clsx(classes.button, {
                 glow: feedbackCTA,
-                [status]: status,
+                [status]: !isMobile && !feedbackCTA ? status : '',
             })}
         >
             <Box style={{ overflow: 'hidden' }} pl={7}>
-                <Text fz="13" lh={1.3} fw="600" className={classes.text} c="black" lineClamp={1}>
+                <Text lh={1.3} fw="600" className={classes.text} c="black" lineClamp={1}>
                     {' '}
                     Send Feedback
                 </Text>
