4b1232a14abd627e6742ff68c6bb0ebf8e7ee3c6
JRzankowski <j.rzankowski@gmail.com>
2024-08-27 15:49:33 +0200

    feat(front): create localStorage handler for x-trace-id

---
+++ frontend/src/utils/getOrCreateTraceId.ts
@@ -0,0 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export function getOrCreateTraceId(): string {
+    let traceId = localStorage.getItem('x-trace-id');
+    if (!traceId) {
+        traceId = uuidv4();
+        localStorage.setItem('x-trace-id', traceId);
+    }
+    return traceId;
+}
