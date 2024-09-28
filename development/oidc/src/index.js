5ba5fb809d2e2fea9f10f1eb785e8c92a80964ae
Waldemar Zieliński <waldemar99zielinski@gmail.com>
2024-09-28 13:24:47 +0200

    feat(dev): oidc test server

---
+++ development/oidc/src/index.js
@@ -0,0 +1,36 @@
+import Provider from 'oidc-provider';
+import express from 'express';
+import { inMemoryStore } from './store.js';
+import { configuration } from './config.js';
+import { getUsers, getUserByEmail, createUser } from './users.js';
+
+configuration.findAccount = async function findAccount(ctx, sub, token) {
+    // use user login as an email
+    const user = inMemoryStore.getUser(sub);
+
+    return {
+        accountId: sub,
+        async claims(use, scope, claims, rejected) {
+            return {
+                sub: user.sub,
+                email: user.email,
+            };
+        }
+    }
+};
+
+const oidc = new Provider('http://oidc/oidc', configuration);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/users', getUsers);
+app.get('/users/:email', getUserByEmail);
+app.post('/users', createUser);
+
+app.use('/oidc', oidc.callback());
+app.listen(3000, () => {
+    console.log('Server started');
+});
